2833b0efcb39a703484c0e1558bfe18e4eb54580
aaldunate <andrealdunatevidaurre@gmail.com>
2023-02-06 09:04:31 -0400

    First commit and branch creation

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.runners.JUnit4;
+import org.junit.runners.model.InitializationError;
+
+public class Main {
+    public static void main(String[] args) throws InitializationError {
+
+        System.out.println("Hello world!");
+
+    }
+}
